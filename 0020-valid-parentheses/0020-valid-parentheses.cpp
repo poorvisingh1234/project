d01a2e50fa1f7d4f9a4fb59182377995cf149922
poorvisingh1234 <poorvisinghh2004@gmail.com>
2025-06-15 17:01:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.63%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char>st;
+        
+        for (char cur : s){
+            if(!st.empty()){
+                char last = st.top();
+                if(isPair(last,cur)){
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(cur);
+        }
+
+        return st.empty();
+
+
+        }
+
+        private:
+            bool isPair(char last, char cur){
+                return (last == '(' && cur == ')') ||
+                    (last =='{' && cur == '}') ||
+                    (last == '[' && cur ==']');
+            }
+    
+};
