94f0d3c04fe80bbcd40a17abcc0e269a791d4c76
poorvisingh1234 <poorvisinghh2004@gmail.com>
2025-06-26 18:53:41 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.46%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -9,4 +9,17 @@ public:
          //Binary:     0 0 0 0  0 0 1 0
          //Bit Index:  7 6 5 4  3 2 1 0
     }
-};
+}; //  // Time Complexity: O(1)
+    // Space Complexity: O(1)
+
+
+
+// bool isPowerOfFour(int n) {
+//     // Time Complexity: O(1)
+//     // Space Complexity: O(1)
+//     if (n <= 0) return false;
+//     double logVal = log10(n) / log10(4);
+//     return floor(logVal) == logVal;
+// }
+// Note: May suffer from floating-point precision issues like 4.9999 will not b eqaul to 5.
+
