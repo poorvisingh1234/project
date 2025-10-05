2bf14bf7cf7cbf14c455d0fd14d30e4ff5c45c03
poorvisingh1234 <poorvisinghh2004@gmail.com>
2025-10-06 01:41:08 +0530

    Time: 47 ms (70.21%), Space: 29 MB (89.84%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> results;
-        sort(nums.begin(), nums.end());
+       int n = nums.size();
+       vector<vector<int>> results;
+       sort(nums.begin(),nums.end());
+       
+       for(int i = 0; i < n ; i++){
+        if (i>0 && nums[i] == nums[i-1]) continue;
+        int j = i + 1;
+        int k = n - 1;
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
-
-            unordered_set<int> seen;
-            for (int j = i + 1; j < n; j++) {
-                int third = -(nums[i] + nums[j]);
-
-                if (seen.count(third)) {
-                    results.push_back({nums[i], nums[j], third});
-
-                    // skip duplicates for nums[j]
-                    while (j + 1 < n && nums[j] == nums[j + 1]) j++;
-                }
-                seen.insert(nums[j]);
+        while (j < k){
+            int sum = nums[i] + nums[k] + nums[j];
+            
+            if (sum < 0){
+                j++;
+            } else if ( sum > 0){
+                k--;
+            } else {
+                results.push_back({nums[i],nums[j],nums[k]});
+                j++; k--;
+                while(j < k && nums[j]==nums[j-1])j++;
             }
+
         }
 
-        // remove duplicate triplets caused by order mismatch
-        for (auto &triplet : results) {
-            sort(triplet.begin(), triplet.end());
-        }
-        sort(results.begin(), results.end());
-        results.erase(unique(results.begin(), results.end()), results.end());
+        
 
-        return results;
+                 
+
+        
+
+       }
+       
+    return results;
+
     }
 };
