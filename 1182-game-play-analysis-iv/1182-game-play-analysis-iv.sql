9d533e97ddc9dd50dd2d250d7b40b3912ee45073
poorvisingh1234 <poorvisinghh2004@gmail.com>
2025-10-13 17:34:12 +0530

    Time: 1768 ms (6.24%), Space: 0B (100%) - LeetHub

---
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -0,0 +1,9 @@
+select 
+round(sum(player_login)/count(distinct player_id),2) as fraction
+from
+(select player_id,
+(datediff(event_date , min(event_date) over (partition by player_id)) = 1) as player_login 
+from Activity) as new_table
+
+
+
