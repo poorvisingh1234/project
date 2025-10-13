9d9d60fe992f67bebc6d2cc01ed2ff4cb8d63114
poorvisingh1234 <poorvisinghh2004@gmail.com>
2025-10-13 17:10:33 +0530

    Time: 515 ms (99.75%), Space: 0B (100%) - LeetHub

---
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -0,0 +1,12 @@
+select 
+round(avg(min_order_date=min_customer_pref_delivery_date)*100,2) as immediate_percentage
+from
+(select delivery_id,
+customer_id,
+min(order_date) as min_order_date,
+min(customer_pref_delivery_date) as min_customer_pref_delivery_date
+from Delivery
+group by customer_id
+) as new_table
+
+
